4c38e4c6a7c32b082307e70f403364bc7c7f5cd6
Zac-HD <zac.hatfield.dodds@gmail.com>
2017-08-25 12:44:31 +1000

    General cleanup of reference code

--- modis_prods/main.py
+++ modis_prods/main.py
@@ -2,9 +2,12 @@ import os.path
 from osgeo import gdal
 import numpy as np
 import argparse
-from utils import pack_data, get_nc_name, get_top_n_functor, get_fmc_functor, get_tile_map_transformer
+from utils import pack_data, get_top_n_functor, get_fmc_functor, \
+    get_tile_map_transformer
 
-def get_fmc_image(rasters, ndvi_raster, auraster, q_mask, trans, x_size, y_size):
+
+def get_fmc_image(rasters, ndvi_raster, auraster,
+                  q_mask, trans, x_size, y_size):
 
     # In case the mask doesn't exist
     if q_mask is None:
@@ -13,10 +16,10 @@ def get_fmc_image(rasters, ndvi_raster, auraster, q_mask, trans, x_size, y_size)
 
     mean_arr = np.zeros((y_size, x_size), dtype=np.float32)
     std_arr = np.zeros((y_size, x_size), dtype=np.float32)
-    
+
     get_top_n = get_top_n_functor()
     get_fmc = get_fmc_functor()
-    
+
     auraster_xsize = auraster.shape[1]
     auraster_ysize = auraster.shape[0]
 
@@ -34,102 +37,78 @@ def get_fmc_image(rasters, ndvi_raster, auraster, q_mask, trans, x_size, y_size)
 
     return mean_arr, std_arr
 
+
 def aggregator(tile_path, map_path, pq_mask_path):
-    
+
     trans = get_tile_map_transformer(tile_path, map_path)
 
     #Special case where we don't use all the bands 1, 2, 4, 6, 7
-    bands = [gdal.Open('HDF4_EOS:EOS_GRID:"{}":MOD_Grid_BRDF:Nadir_Reflectance_Band{}'.format(tile_path, b)) for b in [1, 2, 4, 6, 7]]
+    file_pattern = 'HDF4_EOS:EOS_GRID:"{}":MOD_Grid_BRDF:Nadir_Reflectance_Band{}'
+    bands = [gdal.Open(file_pattern.format(tile_path, b)) for b in [1, 2, 4, 6, 7]]
 
-    #rasters = [band.GetRasterBand(1).ReadAsArray() for band in bands]
-    
     raster_stack = bands[0].GetRasterBand(1).ReadAsArray() * .0001
     for b in range(1, len(bands)):
-        raster_stack = np.dstack((raster_stack, bands[b].GetRasterBand(1).ReadAsArray()*.0001))
+        raster_stack = np.dstack(
+            (raster_stack, bands[b].GetRasterBand(1).ReadAsArray() * .0001))
 
-    #raster_stack = np.dstack((raster_stack, (raster_stack[:, :, 5]-raster_stack[:, :, 1])/(raster_stack[:, :, 5]+raster_stack[:, :, 1])))
     # VDII compositions between bands 2 and 6 -> indexes 1 and 3
-    raster_stack = np.dstack((raster_stack, (raster_stack[:, :, 1]-raster_stack[:, :, 3])/(raster_stack[:, :, 1]+raster_stack[:, :, 3])))
+    raster_stack = np.dstack(
+        (raster_stack,
+         (raster_stack[:, :, 1] - raster_stack[:, :, 3]) / (raster_stack[:, :, 1] + raster_stack[:, :, 3]))
+    )
     #raster stack contains 6 bands: 5 bands from the sat rasters and the VDII one
 
     ndvi_raster = (raster_stack[:, :, 1]-raster_stack[:, :, 0])/(raster_stack[:, :, 1]+raster_stack[:, :, 0])
 
-    #gdal.Open('HDF4_EOS:EOS_GRID:"{}":MOD_Grid_BRDF:Nadir_Reflectance_Band{}'.format(pq_mask_path, b)) for b in [1, 2, 4, 6, 7]]
-    # Call function to compute a boolean aggregate of the bands to be used as mask
-    
     auds = gdal.Open(map_path)
     auraster = auds.GetRasterBand(1).ReadAsArray()
 
     q_mask = quality_mask_composer(pq_mask_path)
- 
+
     x_size, y_size = bands[0].RasterXSize, bands[0].RasterYSize
-    mean_arr, std_arr  = get_fmc_image(raster_stack, ndvi_raster, auraster, q_mask, trans, x_size, y_size)
+    mean_arr, std_arr  = get_fmc_image(raster_stack, ndvi_raster, auraster,
+                                       q_mask, trans, x_size, y_size)
 
     return mean_arr, std_arr
 
 
 def quality_mask_composer(pq_mask_path):
 
-    if os.path.isfile(pq_mask_path):
-
-        albedo = gdal.Open('HDF4_EOS:EOS_GRID:"{}":MOD_Grid_BRDF:BRDF_Albedo_Quality'.format(pq_mask_path))
-        albedo_band = gdal.Open('HDF4_EOS:EOS_GRID:"{}":MOD_Grid_BRDF:BRDF_Albedo_Band_Quality'.format(pq_mask_path))
-        snow = gdal.Open('HDF4_EOS:EOS_GRID:"{}":MOD_Grid_BRDF:Snow_BRDF_Albedo'.format(pq_mask_path))
-
-        albedo_mask = np.equal(albedo.ReadAsArray(), 0)
-        albedo_band_mask = np.equal(albedo_band.ReadAsArray(), 0)
-        snow_mask = np.equal(snow.ReadAsArray(), 0)
-
-        return np.logical_and(np.logical_and(snow_mask, albedo_mask), albedo_band_mask)
-
-    else:
-
+    if not os.path.isfile(pq_mask_path):
         return None
+
+    base = 'HDF4_EOS:EOS_GRID:"{}":MOD_Grid_BRDF:'.format(pq_mask_path)
+    albedo = gdal.Open(base + 'BRDF_Albedo_Quality')
+    albedo_band = gdal.Open(base + 'BRDF_Albedo_Band_Quality')
+    snow = gdal.Open(base + 'Snow_BRDF_Albedo')
+
+    albedo_mask = np.equal(albedo.ReadAsArray(), 0)
+    albedo_band_mask = np.equal(albedo_band.ReadAsArray(), 0)
+    snow_mask = np.equal(snow.ReadAsArray(), 0)
+
+    return np.logical_and(np.logical_and(snow_mask, albedo_mask),
+                          albedo_band_mask)
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="""Modis Vegetation Analysis argument parser""")
-    parser.add_argument(dest="modis_tile", type=str, help="Full path to a modis tile (HDF-EOS).")
-    parser.add_argument(dest="landcover_mask", type=str, help="Full path to the vegetation mask (GeoTIFF).")
-    parser.add_argument(dest="pq_mask", type=str, help="Full path to the pixel quality product mask (HDF-EOS).")
-    parser.add_argument(dest="destination", type=str, help="Full path to destination.")
+    parser = argparse.ArgumentParser(
+        description="""Modis Vegetation Analysis argument parser""")
+    parser.add_argument(
+        dest="modis_tile", type=str,
+        help="Full path to a modis tile (HDF-EOS).")
+    parser.add_argument(
+        dest="landcover_mask", type=str,
+        help="Full path to the vegetation mask (GeoTIFF).")
+    parser.add_argument(
+        dest="pq_mask", type=str,
+        help="Full path to the pixel quality product mask (HDF-EOS).")
+    parser.add_argument(
+        dest="destination", type=str,
+        help="Full path to destination.")
     args = parser.parse_args()
 
-    modis_tile = args.modis_tile
-    landcover_mask = args.landcover_mask
-    pq_mask = args.pq_mask
-    destination = args.destination
+    mean, stdv = aggregator(args.modis_tile, args.landcover_mask, args.pq_mask)
 
-
-    mean, stdv = aggregator(modis_tile, landcover_mask, pq_mask)
-
-    pack_data(modis_tile, mean, stdv, destination)
-   
-
-    """
-    # /g/data1/fj2/MODIS/MCD43A4.005/2010.02.18/
-    tiles = ["MCD43A4.A2010049.h32v10.005.2010068172513.hdf",
-             "MCD43A4.A2010049.h32v11.005.2010068075524.hdf",
-             "MCD43A4.A2010049.h31v10.005.2010068183638.hdf",
-             "MCD43A4.A2010049.h31v11.005.2010068145740.hdf",
-             "MCD43A4.A2010049.h31v12.005.2010068070949.hdf",
-             "MCD43A4.A2010049.h30v10.005.2010068123759.hdf",
-             "MCD43A4.A2010049.h30v11.005.2010068185946.hdf",
-             "MCD43A4.A2010049.h30v12.005.2010068101550.hdf",
-             "MCD43A4.A2010049.h29v10.005.2010068201929.hdf",
-             "MCD43A4.A2010049.h29v11.005.2010068212928.hdf",
-             "MCD43A4.A2010049.h29v12.005.2010068191059.hdf",
-             "MCD43A4.A2010049.h28v10.005.2010068071924.hdf",
-             "MCD43A4.A2010049.h28v11.005.2010068172802.hdf",
-             "MCD43A4.A2010049.h28v12.005.2010068210101.hdf",
-             "MCD43A4.A2010049.h27v11.005.2010068045632.hdf",
-             "MCD43A4.A2010049.h27v12.005.2010068125419.hdf",
-             "MCD43A4.A2010049.h28v13.005.2010068121247.hdf",
-             "MCD43A4.A2010049.h29v13.005.2010068140914.hdf",
-             "MCD43A4.A2010049.h30v13.005.2010068233224.hdf"]
-
-    #/g/data1/fj2/MODIS/MCD43A4.005/2002.09.06/
-    tiles = ["MCD43A4.A2002249.h30v12.005.2007201174136.hdf"]
-    """
+    pack_data(args.modis_tile, mean, stdv, args.destination)
 
--- modis_prods/utils.py
+++ modis_prods/utils.py
@@ -5,119 +5,45 @@ import netCDF4
 import json
 import datetime
 
-"""
-def get_coordinate_mesh(geot, x_size, y_size):
-
-    sin_proj4 = "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs"
-    wgs_proj4 = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
-
-    sin_proj = pyproj.Proj(sin_proj4)
-    wgs_proj = pyproj.Proj(wgs_proj4)
-    
-    x_dim = np.arange(geot[0], geot[0]+(x_size)*geot[1], geot[1])
-    y_dim = np.arange(geot[3], geot[3]+(y_size)*geot[5], geot[5])
-
-    xs = np.repeat(np.expand_dims(x_dim, axis=0), y_dim.shape[0], axis=0)
-    ys = np.repeat(np.expand_dims(y_dim, axis=0), x_dim.shape[0], axis=0).T
-    
-    sin_proj = pyproj.Proj(sin_proj4)
-    wgs_proj = pyproj.Proj(wgs_proj4)
-    
-    longitudes, latitudes = pyproj.transform(sin_proj, wgs_proj, xs.flatten(), ys.flatten())
-    
-    return longitudes.reshape((y_size, x_size)), latitudes.reshape((y_size, x_size))
-
-def pack_data_mesh(hdf_file, mean_arr, std_arr, dest):
-    
-    file_name = get_nc_name(hdf_file, dest)
-    
-    with netCDF4.Dataset(file_name, 'w', format='NETCDF4') as dest:
-        
-        setattr(dest, "Conventions", "CF-1.6")
-        setattr(dest, "Description", "LFMC Modis MCD43A4 Tiles processing. This product was made possible through funding from the Bushfire and Natural Hazards CRC through the project 'Mapping bushfire hazard and impacts'. Contact: marta.yebra@anu.edu.au Reference; Yebra, M., van Dijk, A., Quan, X.,  Cary, G. Monitoring and forecasting fuel moisture content for Australia using a combination of remote sensing and modelling. Proceedings for the 5th International Fire Behaviour and Fuels Conference. April 11-15, 2016, Melbourne, Australia. Published by the International Association of Wildland Fire, Missoula, Montana, USA. (The code used to generate this product was implemented by Pablo Rozas Larraondo at the National Computational Infrastructure)") 
-        ds = gdal.Open('HDF4_EOS:EOS_GRID:"{}":MOD_Grid_BRDF:Nadir_Reflectance_Band1'.format(hdf_file))
-        geot = ds.GetGeoTransform()
-
-        lons, lats = get_coordinate_mesh(geot, ds.RasterXSize, ds.RasterYSize)
-
-        x_dim = dest.createDimension("XDim", ds.RasterXSize)
-        y_dim = dest.createDimension("YDim", ds.RasterYSize)
-
-        var = dest.createVariable("latitude", "f8", ("YDim", "XDim"))
-        var.coordinates = "latitude longitude"
-        var.eoslib = "Calculated latitude"
-        var.units = 'degrees_north'
-        var.long_name = "/MOD_Grid_BRDF/latitude"
-        var.standard_name = "Latitude"
-        var[:] = lats
-
-        var = dest.createVariable("longitude", "f8", ("YDim", "XDim"))
-        var.coordinates = "latitude longitude"
-        var.eoslib = "Calculated longitude"
-        var.units = 'degrees_east'
-        var.long_name = "/MOD_Grid_BRDF/longitude"
-        var.standard_name = "Longitude"
-        var[:] = lons
-        
-        var = dest.createVariable("lfmc_mean", "f4", ("YDim", "XDim"), fill_value=.0)
-        var.coordinates = "latitude longitude"
-        var.long_name = "LFMC Arithmetic Mean"
-        var.units = '%'
-        var[:] = mean_arr
-        
-        var = dest.createVariable("lfmc_stdv", "f4", ("YDim", "XDim"), fill_value=.0)
-        var.coordinates = "latitude longitude"
-        var.long_name = "LFMC Standard Deviation"
-        var.units = '%'
-        var[:] = std_arr
-        
-        var = dest.createVariable("crs", "f4", ())
-        var.projection_name = "GCTP_SNSOID"
-        var.geotransform = geot
-        var.proj4 = "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs"
-"""
 
 def get_nc_name(hdf_tile_path, dest):
-    
     return dest + hdf_tile_path.split("/")[-1][:-4] + "_LFMC.nc"
 
 
 def pack_data(hdf_file, mean_arr, std_arr, dest):
-    
+
     file_name = get_nc_name(hdf_file, dest)
-    
+
     with netCDF4.Dataset(file_name, 'w', format='NETCDF4_CLASSIC') as dest:
         with open('nc_metadata.json') as data_file:
             attrs = json.load(data_file)
-            for key in attrs:
-                setattr(dest, key, attrs[key])
-        setattr(dest, "date_created", datetime.datetime.now().strftime("%Y%m%dT%H%M%S"))
-        
+        for key in attrs:
+            setattr(dest, key, attrs[key])
+        setattr(dest, "date_created",
+                datetime.datetime.now().isoformat(timespec='minutes'))
+
         ds = gdal.Open('HDF4_EOS:EOS_GRID:"{}":MOD_Grid_BRDF:Nadir_Reflectance_Band1'.format(hdf_file))
         proj_wkt = ds.GetProjection()
         geot = ds.GetGeoTransform()
-        
-        x_dim = dest.createDimension("x", ds.RasterXSize)
-        y_dim = dest.createDimension("y", ds.RasterYSize)
 
         var = dest.createVariable("x", "f8", ("x",))
         var.units = "m"
         var.long_name = "x coordinate of projection"
         var.standard_name = "projection_x_coordinate"
         var[:] = np.linspace(geot[0], geot[0]+(geot[1]*ds.RasterXSize), ds.RasterXSize)
-        
+
         var = dest.createVariable("y", "f8", ("y",))
         var.units = "m"
         var.long_name = "y coordinate of projection"
         var.standard_name = "projection_y_coordinate"
         var[:] = np.linspace(geot[3], geot[3]+(geot[5]*ds.RasterYSize), ds.RasterYSize)
-        
+
         var = dest.createVariable("lfmc_mean", 'f4', ("y", "x"), fill_value=.0)
         var.long_name = "LFMC Arithmetic Mean"
         var.units = '%'
         var.grid_mapping = "sinusoidal"
         var[:] = mean_arr
-        
+
         var = dest.createVariable("lfmc_stdv", 'f4', ("y", "x"), fill_value=.0)
         var.long_name = "LFMC Standard Deviation"
         var.units = '%'
@@ -136,19 +62,18 @@ def pack_data(hdf_file, mean_arr, std_arr, dest):
         var.spatial_ref = proj_wkt
         var.GeoTransform = "{} {} {} {} {} {} ".format(*[geot[i] for i in range(6)])
 
+
 # All Modis 7 bands are 2400x2400 so we just get geotransform for Band1
 def get_affine(geot):
-    
-    return np.array([geot[1], .0, geot[0], .0, geot[5], geot[3], .0, .0, 1.]).reshape((3,3))
-    
+    out = [geot[1], 0., geot[0], 0., geot[5], geot[3], 0., 0., 1.]
+    return np.array(out).reshape((3, 3))
+
 
 def transform(geot, i, j):
-    
     return tuple(np.dot(get_affine(geot), np.array([i, j, 1]))[:2])
 
 
 def transform_inv(geot, x, y):
-    
     return tuple(np.dot(np.linalg.inv(get_affine(geot)), np.array([x, y, 1]))[:2])
 
 
@@ -156,27 +81,26 @@ def get_tile_map_transformer(tile_path, map_path):
 
     tile = gdal.Open('HDF4_EOS:EOS_GRID:"{}":MOD_Grid_BRDF:Nadir_Reflectance_Band1'.format(tile_path))
     tile_geot = tile.GetGeoTransform()
-    
+
     aumap = gdal.Open(map_path)
     map_geot = aumap.GetGeoTransform()
-    
 
     def transformer(x, y):
-
         idx = transform_inv(map_geot, *transform(tile_geot, x, y))
         return (int(round(idx[0])), int(round(idx[1])))
-    
+
     return transformer
 
 
 def get_fmc_functor():
     # Load FMC table
     fmc = np.load("./FMC.npy")
-    
+
     def get_fmc(idxs, fmc=fmc):
         # Select Veg type subset from LUT table
-        mean = np.einsum('i->', fmc[idxs])/idxs.shape[0]
-        return mean, np.sqrt(np.einsum('i->',(fmc[idxs]-mean)**2)/idxs.shape[0])
+        mean = np.einsum('i->', fmc[idxs]) / idxs.shape[0]
+        athing = np.einsum('i->', (fmc[idxs] - mean) ** 2) / idxs.shape[0]
+        return mean, np.sqrt(athing)
 
     return get_fmc
 
@@ -205,15 +129,13 @@ def get_top_n_functor():
         vmat = mat[idx[0]:idx[1], :]
         vsmat = smat[idx[0]:idx[1]]
 
-        err = np.arccos(np.einsum('ij,j->i', vmat, mb)/(np.einsum('i,i->', mb, mb)**.5*vsmat))
+        err = np.arccos(
+            np.einsum('ij,j->i', vmat, mb) /
+            (np.einsum('i,i->', mb, mb) ** .5 * vsmat)
+        )
 
         idxs = np.argpartition(err, top_n)[:top_n] + idx[0]
 
         return idxs
 
     return get_top_n
-
-if __name__ == "__main__":
-
-    pass
-
