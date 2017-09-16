b433ce1aebffd9f83790029c72eff9cf4732cb05
Zac-HD <zac.hatfield.dodds@gmail.com>
2017-09-16 21:50:56 +1000

    Run with per-tile walltime requirement

--- launchmany.py
+++ launchmany.py
@@ -5,10 +5,13 @@ with open('tiles.json') as f:
     tiles = json.load(f)
 
 for year in (2016, 2017):
-    for tile in tiles:
+    for tile, walltime in tiles.items():
         fname = '/g/data/ub8/au/FMC/c6/LVMC_{}_{}.nc'.format(year, tile)
         if os.path.isfile(fname):
             print('Already done:', fname)
             continue
         print('Submitting job for', fname)
-        os.system('qsub -v "FMC_YEAR={},FMC_TILE={}" onetile.qsub'.format(year, tile))
+        os.system(
+            'qsub -v "FMC_YEAR={},FMC_TILE={}" -l walltime={} onetile.qsub'
+            .format(year, tile, walltime)
+        )
