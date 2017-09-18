74ba5d6cdccdcf6003a8feafdb77163d274ff346
Zac-HD <zac.hatfield.dodds@gmail.com>
2017-09-18 11:44:37 +1000

    Improve legibility of mass runs

--- launchmany.py
+++ launchmany.py
@@ -5,13 +5,13 @@ with open('tiles.json') as f:
     tiles = json.load(f)
 
 for year in (2016, 2017):
-    for tile, walltime in tiles.items():
+    for tile, walltime in sorted(tiles.items()):
         fname = '/g/data/ub8/au/FMC/c6/LVMC_{}_{}.nc'.format(year, tile)
         if os.path.isfile(fname):
             print('Already done:', fname)
             continue
         print('Submitting job for', fname)
-        os.system(
-            'qsub -v "FMC_YEAR={},FMC_TILE={}" -l walltime={} onetile.qsub'
-            .format(year, tile, walltime)
-        )
+        os.system((
+            'qsub -v "FMC_YEAR={year},FMC_TILE={tile}" '
+            '-l walltime={hours}:00:00 -N {year}{tile}-FMC onetile.qsub'
+        ).format(year=year, tile=tile, hours=walltime))
