ce01e99fcf5277c5333e53b03d1940724e553b31
Zac-HD <zac.hatfield.dodds@gmail.com>
2017-12-08 12:33:12 +1100

    fix typo in means.py

--- means.py
+++ means.py
@@ -5,7 +5,7 @@ with open('tiles.json') as f:
     tiles = list(json.load(f))
 
 for tile in tiles:
-    infile = '/g/data/ub8/au/FMC/LMVC_new/LVMC_20??_{}.nc'.format(tile)
+    infile = '/g/data/ub8/au/FMC/LVMC_new/LVMC_20??_{}.nc'.format(tile)
     outfile = '/g/data/ub8/au/FMC/mean_LVMC_{}.nc'.format(tile)
     xr.open_mfdataset(infile).mean(dim='time').to_netcdf(outfile)
 
