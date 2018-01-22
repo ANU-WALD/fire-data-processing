72c6ad6d6dcdfe630ff2a5b57307efe3a336f224
kingdion <dion.misic@gmail.com>
2018-01-22 15:53:22 +1100

    Ensured all files passed Flake8.

--- lookup_tables/new_modis_lookup.py
+++ lookup_tables/new_modis_lookup.py
@@ -1,18 +1,16 @@
-import xarray as xr
-import numpy as np
 import pandas as pd
 
-import matplotlib.pyplot as plt
-
-# input files are downloaded from: ftp://mcst.ssaihq.com/pub/permanent/MCST/FM1_RSR_LUT_07-10-01/
+# input files are downloaded from:
+# ftp://mcst.ssaihq.com/pub/permanent/MCST/FM1_RSR_LUT_07-10-01/
 
 continuous_lookup = pd.read_csv('continuous_lookup.csv')
 
+
 def create_modis_data(filename):
     data_set = pd.read_fwf('MODIS_Data/' + filename,
-                             comment='#',
-                             skiprows=range(7),
-                             names=['Band', 'Channel', 'Wavelength', 'RSR'])
+                           comment='#',
+                           skiprows=range(7),
+                           names=['Band', 'Channel', 'Wavelength', 'RSR'])
 
     new_series = pd.Series(data_set['RSR'])
     new_series.index = data_set['Wavelength']
@@ -22,7 +20,7 @@ def create_modis_data(filename):
 
     weighted_sum = 0
     for idx in lookup.index:
-        weighted_sum += (lookup[idx] *  continuous_lookup[str(int(idx))])
+        weighted_sum += (lookup[idx] * continuous_lookup[str(int(idx))])
 
     # rename on the basis that files are named 0X.amb.1pct.det
     # where X is the desired band number
@@ -30,10 +28,13 @@ def create_modis_data(filename):
 
     return weighted_sum
 
-files = ['01.amb.1pct.det', '02.amb.1pct.det', '03.amb.1pct.det', '04.amb.1pct.det',
-         '05.tv.1pct.det', '06.tv.1pct.det', '07.tv.1pct.det']
 
-modis_lookup = continuous_lookup[['cab', 'chp', 'cm', 'cw', 'fcov', 'fmc', 'lai', 'landcover']]
+files = ['01.amb.1pct.det', '02.amb.1pct.det', '03.amb.1pct.det',
+         '04.amb.1pct.det', '05.tv.1pct.det', '06.tv.1pct.det',
+         '07.tv.1pct.det']
+
+modis_lookup = continuous_lookup[
+    ['cab', 'chp', 'cm', 'cw', 'fcov', 'fmc', 'lai', 'landcover']]
 for i, file in enumerate(files):
     modis_lookup['Band{}'.format(i)] = create_modis_data(file)
 
