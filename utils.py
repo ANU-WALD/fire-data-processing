e68abf96e7a6b4938dd3909bb05449199b0ad826
prl900 <pablo.larraondo@anu.edu.au>
2018-08-29 13:34:23 +1000

    preparing for reprocessing

--- utils.py
+++ utils.py
@@ -3,86 +3,17 @@ import numpy as np
 import netCDF4
 #import pyproj
 import json
+import os
 import datetime
-
-"""
-def get_coordinate_mesh(geot, x_size, y_size):
-
-    sin_proj4 = "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs"
-    wgs_proj4 = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
-
-    sin_proj = pyproj.Proj(sin_proj4)
-    wgs_proj = pyproj.Proj(wgs_proj4)
-    
-    x_dim = np.arange(geot[0], geot[0]+(x_size)*geot[1], geot[1])
-    y_dim = np.arange(geot[3], geot[3]+(y_size)*geot[5], geot[5])
-
-    xs = np.repeat(np.expand_dims(x_dim, axis=0), y_dim.shape[0], axis=0)
-    ys = np.repeat(np.expand_dims(y_dim, axis=0), x_dim.shape[0], axis=0).T
-    
-    sin_proj = pyproj.Proj(sin_proj4)
-    wgs_proj = pyproj.Proj(wgs_proj4)
-    
-    longitudes, latitudes = pyproj.transform(sin_proj, wgs_proj, xs.flatten(), ys.flatten())
-    
-    return longitudes.reshape((y_size, x_size)), latitudes.reshape((y_size, x_size))
-
-def pack_data_mesh(hdf_file, mean_arr, std_arr, dest):
-    
-    file_name = get_nc_name(hdf_file, dest)
-    
-    with netCDF4.Dataset(file_name, 'w', format='NETCDF4') as dest:
-        
-        setattr(dest, "Conventions", "CF-1.6")
-        setattr(dest, "Description", "LFMC Modis MCD43A4 Tiles processing. This product was made possible through funding from the Bushfire and Natural Hazards CRC through the project 'Mapping bushfire hazard and impacts'. Contact: marta.yebra@anu.edu.au Reference; Yebra, M., van Dijk, A., Quan, X.,  Cary, G. Monitoring and forecasting fuel moisture content for Australia using a combination of remote sensing and modelling. Proceedings for the 5th International Fire Behaviour and Fuels Conference. April 11-15, 2016, Melbourne, Australia. Published by the International Association of Wildland Fire, Missoula, Montana, USA. (The code used to generate this product was implemented by Pablo Rozas Larraondo at the National Computational Infrastructure)") 
-        ds = gdal.Open('HDF4_EOS:EOS_GRID:"{}":MOD_Grid_BRDF:Nadir_Reflectance_Band1'.format(hdf_file))
-        geot = ds.GetGeoTransform()
-
-        lons, lats = get_coordinate_mesh(geot, ds.RasterXSize, ds.RasterYSize)
-
-        x_dim = dest.createDimension("XDim", ds.RasterXSize)
-        y_dim = dest.createDimension("YDim", ds.RasterYSize)
-
-        var = dest.createVariable("latitude", "f8", ("YDim", "XDim"))
-        var.coordinates = "latitude longitude"
-        var.eoslib = "Calculated latitude"
-        var.units = 'degrees_north'
-        var.long_name = "/MOD_Grid_BRDF/latitude"
-        var.standard_name = "Latitude"
-        var[:] = lats
-
-        var = dest.createVariable("longitude", "f8", ("YDim", "XDim"))
-        var.coordinates = "latitude longitude"
-        var.eoslib = "Calculated longitude"
-        var.units = 'degrees_east'
-        var.long_name = "/MOD_Grid_BRDF/longitude"
-        var.standard_name = "Longitude"
-        var[:] = lons
-        
-        var = dest.createVariable("lfmc_mean", "f4", ("YDim", "XDim"), fill_value=.0)
-        var.coordinates = "latitude longitude"
-        var.long_name = "LFMC Arithmetic Mean"
-        var.units = '%'
-        var[:] = mean_arr
-        
-        var = dest.createVariable("lfmc_stdv", "f4", ("YDim", "XDim"), fill_value=.0)
-        var.coordinates = "latitude longitude"
-        var.long_name = "LFMC Standard Deviation"
-        var.units = '%'
-        var[:] = std_arr
-        
-        var = dest.createVariable("crs", "f4", ())
-        var.projection_name = "GCTP_SNSOID"
-        var.geotransform = geot
-        var.proj4 = "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs"
-"""
 
 def get_nc_name(hdf_tile_path, dest):
     
-    return dest + hdf_tile_path.split("/")[-1][:-4] + "_LFMC.nc"
+    fname = hdf_tile_path.split("/")[-1][:-4]
+    fname = ".".join(fname.split(".")[:-1])
+    return os.path.join(dest, fname + ".LFMC.nc")
 
 
-def pack_data(hdf_file, mean_arr, std_arr, dest):
+def pack_data(hdf_file, mean_arr, std_arr, q_mask, dest):
     
     file_name = get_nc_name(hdf_file, dest)
     
@@ -123,9 +54,14 @@ def pack_data(hdf_file, mean_arr, std_arr, dest):
         var.units = '%'
         var.grid_mapping = "sinusoidal"
         var[:] = std_arr
+        
+        var = dest.createVariable("quality_mask", 'i1', ("y", "x"), fill_value=0)
+        var.long_name = "Combined Bands Quality Mask"
+        var.units = 'Cat'
+        var.grid_mapping = "sinusoidal"
+        var[:] = q_mask.astype(np.int8)
 
         var = dest.createVariable("sinusoidal", 'S1', ())
-
         var.grid_mapping_name = "sinusoidal"
         var.false_easting = 0.0
         var.false_northing = 0.0
