cc815b691a3acc07c002b7e88fd4fc5fc1dd2199
prl900 <pablo.larraondo@anu.edu.au>
2018-11-01 10:21:40 +1100

    clean extended with recurrent pbs jobs

--- raijin_tasks/clean.sh
+++ raijin_tasks/clean.sh
@@ -1,5 +1,8 @@
 rm flam.qsub.*
+rm flam_rec.qsub.*
 rm flam_mosaic.qsub.*
+rm flam_mosaic_rec.qsub.*
 rm fmc.qsub.*
-rm fmc_mosaic.qsub.*
 rm fmc_rec.qsub.*
+rm fmc_mosaic.qsub.*
+rm fmc_mosaic_rec.qsub.*
