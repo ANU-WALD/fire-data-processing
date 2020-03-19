cdcfaee89b4ecb9833324417cf268e3c800a061d
Pablo Larraondo <pl5189@gadi-login-02.gadi.nci.org.au>
2020-03-19 13:22:51 +1100

    modified flam qsub scripts for gadi

--- gadi_tasks/flam_qsub.py
+++ gadi_tasks/flam_qsub.py
@@ -11,5 +11,6 @@ if __name__ == "__main__":
 
 
     for au_tile in au_tiles:
-        print("qsub -v 'year={0},tile={1}' flam_rec.qsub".format(args.year, au_tile))
-        os.system('qsub -v "year={0},tile={1}" flam_rec.qsub'.format(args.year, au_tile))
+        print("qsub -v 'year={0},tile={1}' flam.qsub".format(args.year, au_tile))
+        #os.system('qsub -v "year={0},tile={1}" flam_rec.qsub'.format(args.year, au_tile))
+        os.system('qsub -v "year={0},tile={1}" flam.qsub'.format(args.year, au_tile))
