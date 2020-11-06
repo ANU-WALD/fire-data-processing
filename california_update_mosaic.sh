c81644df784218d09714ad71ba5297fdbcdab0b0
Gianluca Scortechini <gs3146@vdi-n16.nci.org.au>
2020-11-06 18:31:36 +1100

    folder non_australia

--- california_update_mosaic.sh
+++
@@ -1,6 +0,0 @@
-module load cdo
-
-/g/data/xc0/software/conda-envs/rs3/bin/python california_update_fmc_mosaic.py -y 20 -dst /g/data/xc0/user/scortechini/cal_lfmc/mosaics/fmc_c6_20.nc -tmp /g/data/xc0/user/scortechini/tmp2/
-https://anu365-my.sharepoint.com/:u:/g/personal/u1059880_anu_edu_au/EWwS2cfQkipNtcDWSIdYwb0BKFRq2sLFaIfenFAdvD0flw?e=ZpA6ks
-
-
