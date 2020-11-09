f3983b381c77f94752710355bc5b9abb6d3c25d2
Gianluca Scortechini <gs3146@vdi-n25.nci.org.au>
2020-11-09 23:06:45 +1100

    geotransform fmc and flam tiles corrected

--- cdo_mean.py
+++ cdo_mean.py
@@ -2,8 +2,8 @@ import os
 import uuid 
 import argparse
 
-fmc_tile = "/g/data/fj4/scratch/fmc_c6_{}_{}.nc"
-mean_tile = "/g/data/fj4/scratch/mean_2001_2016_{}.nc"
+fmc_tile = "/g/data/ub8/au/FMC/c6/fmc_c6_{}_{}.nc"
+mean_tile = "/g/data/xc0/user/scortechini/tmp_mean_fmc/mean_2001_2019_{}.nc"
 au_tiles = ["h27v11", "h27v12", "h28v11", "h28v12", "h28v13", "h29v10", "h29v11", "h29v12", "h29v13", "h30v10", "h30v11", "h30v12", "h31v10", "h31v11", "h31v12", "h32v10", "h32v11"]
 
 if __name__ == "__main__":
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     for tile_id in au_tiles:
-        fmc_tiles = [fmc_tile.format(year, tile_id) for year in range(2001,2017)]
+        fmc_tiles = [fmc_tile.format(year, tile_id) for year in range(2001,2020)]
 
         tmp_file = os.path.join(args.tmp, "{}.nc".format(uuid.uuid4().hex))
         dst_file = mean_tile.format(tile_id)
