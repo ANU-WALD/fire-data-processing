47e79e08c6fa50a76f27d7fe49f4b816d0e7c506
Gianluca Scortechini <gs3146@vdi-n27.nci.org.au>
2020-12-14 18:14:55 +1100

    deciles updated

---
+++ deciles/deciles.sh
@@ -0,0 +1,3 @@
+/g/data/xc0/software/python/miniconda3/bin/python3 04_zonal_statistics_org_nc.py /g/data/xc0/project/FMC_Australia/calc_deciles/data/temp flammability 2018 LGA
+/g/data/xc0/software/python/miniconda3/bin/python3 04_zonal_statistics_org_nc.py /g/data/xc0/project/FMC_Australia/calc_deciles/data/temp flammability 2019 LGA
+/g/data/xc0/software/python/miniconda3/bin/python3 04_zonal_statistics_org_nc.py /g/data/xc0/project/FMC_Australia/calc_deciles/data/temp flammability 2020 LGA
