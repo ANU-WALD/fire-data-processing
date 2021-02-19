03aabf8a8f220287741f52711bbc7a8e46ce8fc3
Gianluca Scortechini <gs3146@vdi-n12.nci.org.au>
2021-02-20 09:38:21 +1100

    fixed recurrent deciles

--- gadi_tasks/au_recurrent_qsubs/au_stats_and_deciles_qsub_rec.py
+++ gadi_tasks/au_recurrent_qsubs/au_stats_and_deciles_qsub_rec.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         if os.path.isfile('/g/data/ub8/au/FMC/c6/mosaics/deciles/fmc_c6_{}_dc.nc'.format(year)):
             deciles = xr.open_dataset('/g/data/ub8/au/FMC/c6/mosaics/deciles/fmc_c6_{}_dc.nc'.format(year))
             lfmc = xr.open_dataset('/g/data/ub8/au/FMC/c6/mosaics/fmc_c6_{}.nc'.format(year))
-            if deciles.time.data[-1] == lfmc.time.data[-1]:
+            if len(deciles.time.data) == len(lfmc.time.data):
                 os.system('qsub /g/data/xc0/user/scortechini/github/fire-data-processing/gadi_tasks/au_recurrent_qsubs/for_not_running_dec_every_day.qsub')
             else:
                 os.system('qsub -v "year={0}" /g/data/xc0/user/scortechini/github/fire-data-processing/gadi_tasks/au_recurrent_qsubs/au_stats_and_deciles_rec.qsub'.format(year))
