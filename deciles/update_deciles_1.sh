68605e1e08a0857cbddb949024c53528488fccb2
Gianluca Scortechini <gs3146@vdi-n12.nci.org.au>
2021-02-19 23:08:49 +1100

    recurrent jobs for deciles adjusted

--- deciles/update_deciles_1.sh
+++
@@ -1,4 +0,0 @@
-
-/g/data/xc0/software/python/miniconda3/bin/python3 03_calc_single_year_deciles.py flammability /g/data/xc0/project/FMC_Australia/calc_deciles/data/temp 2020
-
-/g/data/xc0/software/python/miniconda3/bin/python3 03_calc_single_year_deciles.py fmc_mean /g/data/xc0/project/FMC_Australia/calc_deciles/data/temp 2020
