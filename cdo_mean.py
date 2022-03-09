5cfed7f0d1ed8a7b231ada6633d237534c65a5a7
Gianluca Scortechini <gs3146@ood-vn1.z48>
2022-03-09 21:27:37 +1100

    updated paths

--- cdo_mean.py
+++ cdo_mean.py
@@ -2,8 +2,8 @@ import os
 import uuid 
 import argparse
 
-fmc_tile = "/g/data/ub8/au/FMC/median_LFMC/fmc_c6_{}_{}.nc"
-mean_tile = "/g/data/ub8/au/FMC/median_LFMC/mean_2001_2021_{}.nc"
+fmc_tile = "/g/data/ub8/au/FMC/c6/fmc_c6_{}_{}.nc"
+mean_tile = "/g/data/ub8/au/FMC/c6/mean_2001_2021_{}.nc"
 au_tiles = ["h27v11", "h27v12", "h28v11", "h28v12", "h28v13", "h29v10", "h29v11", "h29v12", "h29v13", "h30v10", "h30v11", "h30v12", "h31v10", "h31v11", "h31v12", "h32v10", "h32v11"]
 
 if __name__ == "__main__":
