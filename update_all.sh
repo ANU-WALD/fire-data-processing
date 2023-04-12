ecb6cc57f7eb77a37b3ba2f5f152e56b87d8c3d7
Gianluca Scortechini <scortechini.gianluca@gmail.com>
2023-04-12 20:29:35 +1000

    test

--- update_all.sh
+++ update_all.sh
@@ -1,5 +1,7 @@
 module load cdo
 
+cd /g/data/xc0/user/scortechini/github/fire-data-processing/main_lfmc_flam
+
 /g/data/xc0/software/conda-envs/rs3/bin/python update_fmc.py -d 2023 -t h27v11 -dst /g/data/ub8/au/FMC/tiles/fmc_c6_2023_h27v11.nc -tmp /g/data/ub8/au/FMC/tmp/
 /g/data/xc0/software/conda-envs/rs3/bin/python update_fmc.py -d 2023 -t h27v12 -dst /g/data/ub8/au/FMC/tiles/fmc_c6_2023_h27v12.nc -tmp /g/data/ub8/au/FMC/tmp/
 /g/data/xc0/software/conda-envs/rs3/bin/python update_fmc.py -d 2023 -t h28v11 -dst /g/data/ub8/au/FMC/tiles/fmc_c6_2023_h28v11.nc -tmp /g/data/ub8/au/FMC/tmp/
@@ -41,11 +43,10 @@ module load cdo
 /g/data/xc0/software/conda-envs/rs3/bin/python update_flammability_mosaic.py -y 2023 -dst /g/data/ub8/au/FMC/mosaics/flam_c6_2023.nc -tmp /g/data/ub8/au/FMC/tmp/
 
 
+cd /g/data/xc0/user/scortechini/github/fire-data-processing/deciles
 
-/g/data/xc0/software/conda-envs/rs3/bin/python /g/data/xc0/user/scortechini/github/fire-data-processing/deciles/zonalstats_update_rank_with_deciles.py -decfolder /g/data/ub8/au/FMC/intermediary_files/deciles_arrays -mosfolder /g/data/ub8/au/FMC/mosaics -var both -ystart 2023 -yend 2023 -outfolder /g/data/ub8/au/FMC/stats -tmpfolder /g/data/ub8/au/FMC/tmp
+/g/data/xc0/software/conda-envs/rs3/bin/python zonalstats_update_rank_with_deciles.py -decfolder /g/data/ub8/au/FMC/intermediary_files/deciles_arrays -mosfolder /g/data/ub8/au/FMC/mosaics -var both -ystart 2023 -yend 2023 -outfolder /g/data/ub8/au/FMC/stats -tmpfolder /g/data/ub8/au/FMC/tmp
 
+/g/data/xc0/software/conda-envs/rs3/bin/python zonalstats_zonal_stats_absolute.py -mosfolder /g/data/ub8/au/FMC/mosaics -vegmaskfolder /g/data/ub8/au/FMC/intermediary_files/vegetation_mask -areafolder /g/data/ub8/au/FMC/intermediary_files/areal_classifications -area both -var both -ystart 2023 -yend 2023 -outfolder /g/data/ub8/au/FMC/stats/zonal_stats -tmpfolder /g/data/ub8/au/FMC/tmp
 
-/g/data/xc0/software/conda-envs/rs3/bin/python /g/data/xc0/user/scortechini/github/fire-data-processing/deciles/zonalstats_zonal_stats_absolute.py -mosfolder /g/data/ub8/au/FMC/mosaics -vegmaskfolder /g/data/ub8/au/FMC/intermediary_files/vegetation_mask -areafolder /g/data/ub8/au/FMC/intermediary_files/areal_classifications -area both -var both -ystart 2023 -yend 2023 -outfolder /g/data/ub8/au/FMC/stats/zonal_stats -tmpfolder /g/data/ub8/au/FMC/tmp
-
-
-/g/data/xc0/software/conda-envs/rs3/bin/python /g/data/xc0/user/scortechini/github/fire-data-processing/deciles/zonalstats_zonal_stats_relative.py -decfolder /g/data/ub8/au/FMC/stats -vegmaskfolder /g/data/ub8/au/FMC/intermediary_files/vegetation_mask -areafolder /g/data/ub8/au/FMC/intermediary_files/areal_classifications -area both -var both -ystart 2023 -yend 2023 -outfolder /g/data/ub8/au/FMC/stats/zonal_stats -tmpfolder /g/data/ub8/au/FMC/tmp
+/g/data/xc0/software/conda-envs/rs3/bin/python zonalstats_zonal_stats_relative.py -decfolder /g/data/ub8/au/FMC/stats -vegmaskfolder /g/data/ub8/au/FMC/intermediary_files/vegetation_mask -areafolder /g/data/ub8/au/FMC/intermediary_files/areal_classifications -area both -var both -ystart 2023 -yend 2023 -outfolder /g/data/ub8/au/FMC/stats/zonal_stats -tmpfolder /g/data/ub8/au/FMC/tmp
