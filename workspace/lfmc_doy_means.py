c136389ec2eaf1fd96b3138a48792e8a5d2fda8b
Gianluca S <34927264+GL-S@users.noreply.github.com>
2023-08-25 16:46:05 +1000

    improved code doy means

--- workspace/lfmc_doy_means.py
+++ workspace/lfmc_doy_means.py
@@ -1,6 +1,6 @@
 import xarray as xr
 import numpy as np
-import pandas as pd
+from datetime import datetime
 
 
 lfmc_days_of_year = [i for i in range(1,366,4)]
@@ -12,29 +12,22 @@ year_end = 2022
 
 for au_tile in au_tiles:
     for doy in lfmc_days_of_year:
+        print(au_tile, doy)
 
         list_lfmc_same_doy = list()
         
         for year in range(year_start,year_end+1):
 
-            tile = xr.open_dataset('/g/data/ub8/au/FMC/tiles/fmc_c6_{0}_{1}.nc'.format(year, au_tile))
+            tile = xr.open_dataset('/g/data/ub8/au/FMC/tiles/fmc_c6_{0}_{1}.nc'.format(year, au_tile)) 
 
-            for d in tile.time.data:
-
-                doy_d = pd.to_datetime(d).dayofyear
-
-                if doy_d == doy:
-
-                    print('adding doy array ', au_tile, year, d, doy)
-
-                    lfmc_array = tile.sel(time=d).lfmc_median.data
-                    list_lfmc_same_doy.append(lfmc_array)
+            d = datetime.strptime('{0}{1}'.format(year,doy), '%Y%j').strftime('%Y-%m-%d')
+            print(d)
+            lfmc_array = tile.sel(time=d).lfmc_median.data
+            
+            list_lfmc_same_doy.append(lfmc_array)
 
         lfmc_same_doy_3d = np.dstack(list_lfmc_same_doy)
         lfmc_same_doy_mean = np.nanmean(lfmc_same_doy_3d, axis=2)
         np.savez_compressed('/g/data/ub8/au/FMC/mean_lfmc_arrays/mean_lfmc_{0}_{1}_{2}_{3}.npz'.format(year_start, year_end, au_tile, doy), mean_lfmc=lfmc_same_doy_mean)
 
 print('done')
-
-
-
